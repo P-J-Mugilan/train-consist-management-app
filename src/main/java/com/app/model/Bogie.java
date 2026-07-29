b1ca572c40c20bae543f10bf6f738174b23e271c
P J Mugilan <pjmugilan@gmail.com>
2026-07-29 11:58:26 +0530

    UC7: Implement capacity sorting

---
+++ src/main/java/com/app/model/Bogie.java
@@ -0,0 +1,57 @@
+package com.app.model;
+
+public class Bogie {
+    private String id;
+    private String name;
+    private String type;
+    private int capacity;
+
+    public Bogie(String id, String name, String type, int capacity) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.capacity = capacity;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    @Override
+    public String toString() {
+        return "Bogie{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", type='" + type + '\'' +
+                ", capacity=" + capacity +
+                '}';
+    }
+}
